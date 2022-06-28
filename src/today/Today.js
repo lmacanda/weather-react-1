17a329c9375b10ec596316f5d0b2d0b20ab837aa
lmacanda <laurina02@hotmail.com>
2022-06-28 20:47:11 +0100

    import axios + api key and url

--- src/today/Today.js
+++ src/today/Today.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./today.css";
 import { RiTempHotLine } from "react-icons/ri";
+import axios from "axios";
 
 export default function Today() {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  const apiKey = "46b29bb09ed0ef009d3c22278289179e";
+  let city = "lisbon";
+  let units = "metric";
+  let apiEndPoint = "https://api.openweathermap.org/data/2.5/weather?q=";
+  let apiUrl = `${apiEndPoint}${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="container container__today">
       <h1>Lisbon</h1>
